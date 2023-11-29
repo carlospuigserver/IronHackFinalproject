48e9c3eff3d3741207e1a7049da619cc888dc393
carlospuigserver <cpuiggut@myuax.com>
2023-11-29 04:41:53 +0100

    c

--- script.js
+++ script.js
@@ -1,120 +1,125 @@
-// Cargo en un arreglo las imágenes de los futbolistas. Este será el orden que se mostrarán.
-let futbolistas = [ "ronaldo.jpg","messi.jpg", "neymar.jpg", "aguirretxe.jpg", "duda.jpg", "samper.jpg", "douglas.jpg", "granero.jpg", "altintop.jpg", "mathieu.jpg"];
-
-// Arreglo que guardará la opción correcta para cada futbolista.
-let correcta = [1, 1, 2, 1, 0, 1, 1, 1, 1, 1];
-
-// Arreglo que guardará los nombres de los futbolistas a mostrar en cada jugada.
-let opciones = [];
-// Cargo en el arreglo opciones los nombres a mostrar en cada jugada.
-opciones.push(["Figo", "Cristiano Ronaldo", "Ronaldo Nazario"]);
-opciones.push(["Maradona", "Messi", "Riquelme"]);
-opciones.push(["Garrincha", "Romario", "Neymar"]);
-opciones.push(["Zurutuza", "Aguirretxe", "Xabi Prieto"]);
-opciones.push(["Duda", "Hélder Postiga", "Raúl Meireles"]);
-opciones.push(["Montoya", "Samper", "Aleñà"]);
-opciones.push(["Adriano", "Douglas", "Paulinho"]);
-opciones.push(["Canales", "Granero", "Negredo"]);
-opciones.push(["Sokratis", "Altintop", "Konoplyanka"]);
-opciones.push(["Lenglet", "Mathieu", "Umtiti"]);
-
-// Variable que guarda la posición actual.
-let posActual = 0;
-// Variable que guarda la cantidad acertadas hasta el momento.
-let cantidadAcertadas = 0;
-
-function comenzarJuego() {
+// Arreglo que guarda las imágenes de los futbolistas. Este será el orden que se mostrarán.
+let futbolistas = [
+    "ronaldo.jpg", "messi.jpg", "neymar.jpg", "aguirretxe.jpg", "duda.jpg",
+    "samper.jpg", "douglas.jpg", "granero.jpg", "altintop.jpg", "mathieu.jpg",
+    "arshavin.jpg", "fred.jpg", "guarín.avif", "el-arabi.jpg", "kanoute.jpg",
+    "redondo.jpg", "eusebio.jpg", "verón.jpg", "güiza.jpg", "lescott.jpg"
+  ];
+  
+  // Arreglo que guarda la opción correcta para cada futbolista.
+  let correcta = [1, 1, 2, 1, 0, 1, 1, 1, 1, 1, 1, 1, 2, 1, 0, 1, 1, 1, 1, 1];
+  
+  // Arreglo que guarda los nombres de los futbolistas a mostrar en cada jugada.
+  let opciones = [
+    ["Figo", "Cristiano Ronaldo", "Ronaldo Nazario"],
+    ["Maradona", "Messi", "Riquelme"],
+    ["Garrincha", "Romario", "Neymar"],
+    ["Zurutuza", "Aguirretxe", "Xabi Prieto"],
+    ["Duda", "Hélder Postiga", "Raúl Meireles"],
+    ["Montoya", "Samper", "Aleñà"],
+    ["Adriano", "Douglas", "Paulinho"],
+    ["Canales", "Granero", "Negredo"],
+    ["Sokratis", "Altintop", "Konoplyanka"],
+    ["Lenglet", "Mathieu", "Umtiti"]
+  ];
+  
+  // Arreglo que guarda los nombres correctos de los futbolistas del 11 al 20.
+  let nombresCorrectos2 = ["Arshavin", "Fred", "Guarín", "El Arabi", "Kanouté", "Redondo", "Eusébio", "Verón", "Güiza", "Lescott"];
+  
+  // Variable que guarda la posición actual.
+  let posActual = 0;
+  
+  // Variable que guarda la cantidad acertadas hasta el momento.
+  let cantidadAcertadas = 0;
+  
+  // Función que inicia el juego.
+  function comenzarJuego() {
     // Reseteamos las variables.
     posActual = 0;
     cantidadAcertadas = 0;
-    // Activamos las pantallas necesarias.
+    
+    // Ocultamos la pantalla inicial y mostramos la pantalla del juego.
     document.getElementById("pantalla-inicial").style.display = "none";
     document.getElementById("pantalla-juego").style.display = "block";
+    
+    // Cargamos el primer futbolista.
     cargarFutbolista();
-}
-
-// Función que carga el siguiente futbolista y sus opciones.
-function cargarFutbolista() {
+  }
+  
+  // Función que carga el siguiente futbolista y sus opciones.
+  function cargarFutbolista() {
     // Controlamos si se han acabado los futbolistas.
     if (futbolistas.length <= posActual) {
-        terminarJuego();
+      terminarJuego();
     } else {
-        // Limpiamos las clases que se asignaron.
-        limpiarOpciones();
-
-        document.getElementById("imgFutbolista").src = "img/" + futbolistas[posActual];
-        document.getElementById("n0").innerHTML = opciones[posActual][0];
-        document.getElementById("n1").innerHTML = opciones[posActual][1];
-        document.getElementById("n2").innerHTML = opciones[posActual][2];
+      // Limpiamos las clases que se asignaron.
+      limpiarOpciones();
+  
+      // Mostramos la imagen y las opciones del futbolista actual.
+      document.getElementById("imgFutbolista").src = "img/" + futbolistas[posActual];
+      document.getElementById("n0").innerHTML = opciones[posActual][0];
+      document.getElementById("n1").innerHTML = opciones[posActual][1];
+      document.getElementById("n2").innerHTML = opciones[posActual][2];
     }
-}
-
-function limpiarOpciones() {
-    document.getElementById("n0").className = "nombre";
-    document.getElementById("n1").className = "nombre";
-    document.getElementById("n2").className = "nombre";
-
-    document.getElementById("l0").className = "letra";
-    document.getElementById("l1").className = "letra";
-    document.getElementById("l2").className = "letra";
-}
-
-// ... (tu código existente)
-
-function comprobarRespuesta(opElegida) {
+  }
+  
+  // Función que limpia las clases de las opciones.
+  function limpiarOpciones() {
+    for (let i = 0; i < 3; i++) {
+      document.getElementById("n" + i).className = "nombre";
+      document.getElementById("l" + i).className = "letra";
+      document.getElementById("op" + i).classList.remove("correcta", "incorrecta");
+    }
+  }
+  
+  // Función que comprueba la respuesta seleccionada.
+  function comprobarRespuesta(opElegida) {
     // Limpiamos las clases que se asignaron anteriormente.
     limpiarOpciones();
-
+  
+    // Comparamos la opción elegida con la opción correcta.
     if (opElegida == correcta[posActual]) { // Acertó.
-        // Agregamos las clases para colocar el color verde a la opción elegida.
-        document.getElementById("n" + opElegida).classList.add("nombreAcertada");
-        document.getElementById("l" + opElegida).classList.add("letraAcertada");
-        document.getElementById("op" + opElegida).classList.add("correcta");
-        cantidadAcertadas++;
+      // Agregamos las clases para resaltar la opción elegida.
+      document.getElementById("n" + opElegida).classList.add("nombreAcertada");
+      document.getElementById("l" + opElegida).classList.add("letraAcertada");
+      document.getElementById("op" + opElegida).classList.add("correcta");
+      cantidadAcertadas++;
     } else { // No acertó.
-        // Agregamos las clases para colocar en rojo la opción elegida.
-        document.getElementById("n" + opElegida).classList.add("nombreNoAcertada");
-        document.getElementById("l" + opElegida).classList.add("letraNoAcertada");
-        document.getElementById("op" + opElegida).classList.add("incorrecta");
-
-        // Opción que era correcta.
-        document.getElementById("n" + correcta[posActual]).classList.add("nombreAcertada");
-        document.getElementById("l" + correcta[posActual]).classList.add("letraAcertada");
-        document.getElementById("op" + correcta[posActual]).classList.add("correcta");
+      // Agregamos las clases para resaltar la opción incorrecta y la correcta.
+      document.getElementById("n" + opElegida).classList.add("nombreNoAcertada");
+      document.getElementById("l" + opElegida).classList.add("letraNoAcertada");
+      document.getElementById("op" + opElegida).classList.add("incorrecta");
+  
+      document.getElementById("n" + correcta[posActual]).classList.add("nombreAcertada");
+      document.getElementById("l" + correcta[posActual]).classList.add("letraAcertada");
+      document.getElementById("op" + correcta[posActual]).classList.add("correcta");
     }
-
+  
     // Esperamos 1 segundo y limpiamos las clases.
     setTimeout(limpiarOpciones, 1000);
     
+    // Pasamos al siguiente futbolista.
     posActual++;
-    // Esperamos 1 segundo adicional y pasamos a mostrar la siguiente futbolista y sus opciones.
+    // Esperamos 1 segundo adicional y cargamos el siguiente futbolista.
     setTimeout(cargarFutbolista, 1000);
-}
-
-function limpiarOpciones() {
-    // Limpiamos las clases de todas las opciones.
-    for (let i = 0; i < 3; i++) {
-        document.getElementById("n" + i).className = "nombre";
-        document.getElementById("l" + i).className = "letra";
-        document.getElementById("op" + i).classList.remove("correcta", "incorrecta");
-    }
-}
-
-
-
-
-function terminarJuego() {
-    // Ocultamos las pantallas y mostramos la pantalla final.
+  }
+  
+  // Función que termina el juego y muestra los resultados.
+  function terminarJuego() {
+    // Ocultamos la pantalla del juego y mostramos la pantalla final.
     document.getElementById("pantalla-juego").style.display = "none";
     document.getElementById("pantalla-final").style.display = "block";
-    // Agregamos los resultados.
+    
+    // Mostramos los resultados.
     document.getElementById("numCorrectas").innerHTML = cantidadAcertadas;
     document.getElementById("numIncorrectas").innerHTML = futbolistas.length - cantidadAcertadas;
-}
-
-function volverAlInicio() {
-    // Ocultamos las pantallas y activamos la inicial.
+  }
+  
+  // Función que reinicia el juego.
+  function volverAlInicio() {
+    // Ocultamos la pantalla final y mostramos la pantalla inicial.
     document.getElementById("pantalla-final").style.display = "none";
     document.getElementById("pantalla-inicial").style.display = "block";
     document.getElementById("pantalla-juego").style.display = "none";
-}
+  }
+  
