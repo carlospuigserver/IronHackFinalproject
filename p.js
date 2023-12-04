66c0af32b2c215bacc9e989370703382053be1ed
carlospuigserver <cpuiggut@myuax.com>
2023-12-04 02:36:01 +0100

    c

--- p.js
+++ p.js
@@ -4,7 +4,7 @@ let futbolistasNivel1 = [
     "el-arabi.jpg", "douglas.jpg", "granero.jpg", "altintop.jpg", "malouda.jpg", "güiza.jpg",
 ];
 
-let correctaNivel1 = [1, 1, 2, 1, 0, 1, 2, 1, 1, 0,2];
+let correctaNivel1 = [1, 1, 2, 1, 0, 1, 2, 1, 1, 0, 2];
 
 let opcionesNivel1 = [
     ["Figo", "Cristiano Ronaldo", "Ronaldo Nazario"],
@@ -35,19 +35,33 @@ let futbolistasNivel2 = [
     { nombre: "Borja Valero", imagen: "img/borja-valero.jpg" },
     { nombre: "Mathieu", imagen: "img/mathieu.jpg" },
     { nombre: "Eliseu", imagen: "img/eliseu.jpg" },
-    
-
-    
-    
-
-
-
-
 ];
 
 let indiceFutbolistaNivel2 = 0;
 let intentosCorrectosNivel2 = 0;
 let intentosIncorrectosNivel2 = 0;
+
+// Nivel 3
+let futbolistasNivel3 = [
+    { nombre: "Zidane", imagen: "img/zidane.jpg" },
+    { nombre: "Baresi", imagen: "ruta de la imagen" }, // Agrega la ruta de la imagen correspondiente
+    { nombre: "Laudrup", imagen: "ruta de la imagen" },
+    { nombre: "Beckham", imagen: "ruta de la imagen" },
+    { nombre: "Bergkamp", imagen: "ruta de la imagen" },
+    { nombre: "Redondo", imagen: "ruta de la imagen" },
+    { nombre: "Puskás", imagen: "ruta de la imagen" },
+    { nombre: "Luis Suárez", imagen: "ruta de la imagen" },
+    { nombre: "Zico", imagen: "ruta de la imagen" },
+    { nombre: "Platini", imagen: "ruta de la imagen" },
+    { nombre: "Weah", imagen: "ruta de la imagen" },
+    { nombre: "Cantona", imagen: "ruta de la imagen" },
+    // Agrega más futbolistas según sea necesario
+];
+
+
+let indiceFutbolistaNivel3 = 0;
+let intentosCorrectosNivel3 = 0;
+let intentosIncorrectosNivel3 = 0;
 
 // Funciones generales
 function comenzarJuegoNivel1() {
@@ -135,7 +149,7 @@ function mostrarPantallaFinalNivel1() {
 
 function terminarJuegoNivel1() {
     document.getElementById("pantalla-juego").style.display = "none";
-    document.getElementById("pantalla-nivel-2").style.display = "block";
+    document.getElementById("pantalla-inicial-nivel-2").style.display = "block";
 }
 
 function irAlSiguienteNivel() {
@@ -225,16 +239,117 @@ function mostrarPantallaFinalNivel2() {
     pantallaFinalNivel2.appendChild(mensajeFinalNivel2);
 }
 
-function volverAlInicioNivel2() {
-    indiceFutbolistaNivel2 = 0;
-    intentosCorrectosNivel2 = 0;
-    intentosIncorrectosNivel2 = 0;
 
-    document.getElementById("pantalla-final-nivel-2").style.display = "none";
-    document.getElementById("pantalla-inicial").style.display = "block";
+
+function terminarJuegoNivel2() {
+    // Oculta la pantalla de juego del Nivel 2
+    document.getElementById("pantalla-juego-nivel-2").style.display = "none";
+    
+    // Muestra la pantalla final del Nivel 2
+    document.getElementById("pantalla-final-nivel-2").style.display = "block";
+
+    // Llama a la función para ir al siguiente nivel (Nivel 3)
+    irAlSiguienteNivel3();
 }
 
+// Otra opción sería llamar a irAlSiguienteNivel3 desde el botón de la pantalla final del Nivel 2.
+
+// Función para ir al tercer nivel
+function irAlSiguienteNivel3() {
+    // Oculta la pantalla final del segundo nivel
+    document.getElementById("pantalla-final-nivel-2").style.display = "none";
+
+    // Muestra la pantalla inicial del tercer nivel
+    document.getElementById("pantalla-inicial-nivel-3").style.display = "block";
+}
+
+
+function ocultarMostrarPantallas(pantallaOculta, pantallaVisible) {
+    document.getElementById(pantallaOculta).style.display = "none";
+    document.getElementById(pantallaVisible).style.display = "block";
+}
+
+
+
+
+// Funciones para el Nivel 3
+
+function comenzarJuegoNivel3() {
+    indiceFutbolistaNivel3 = 0;
+    intentosCorrectosNivel3 = 0;
+    intentosIncorrectosNivel3 = 0;
+
+    ocultarMostrarPantallas("pantalla-inicial-nivel-3", "pantalla-juego-nivel-3");
+
+    mostrarFutbolistaNivel3();
+}
+
+function mostrarFutbolistaNivel3() {
+    const imgElementNivel3 = document.getElementById("imgFutbolistaNivel3");
+    imgElementNivel3.src = futbolistasNivel3[indiceFutbolistaNivel3].imagen;
+    document.getElementById("nombreFutbolistaNivel3").value = "";
+    document.getElementById("nombreFutbolistaNivel3").focus();
+}
+
+function comprobarRespuestaNivel3() {
+    const nombreIngresadoNivel3 = document.getElementById("nombreFutbolistaNivel3").value.trim();
+    const nombreCorrectoNivel3 = futbolistasNivel3[indiceFutbolistaNivel3].nombre;
+
+    const nombreInputNivel3 = document.getElementById("nombreFutbolistaNivel3");
+    if (nombreIngresadoNivel3.toLowerCase() === nombreCorrectoNivel3.toLowerCase()) {
+        intentosCorrectosNivel3++;
+        nombreInputNivel3.parentElement.classList.add("correcto");
+    } else {
+        intentosIncorrectosNivel3++;
+        nombreInputNivel3.parentElement.classList.add("incorrecto");
+    }
+
+    setTimeout(() => {
+        nombreInputNivel3.parentElement.classList.remove("correcto", "incorrecto");
+        siguienteFutbolistaNivel3();
+    }, 1000);
+}
+
+function siguienteFutbolistaNivel3() {
+    indiceFutbolistaNivel3++;
+
+    if (indiceFutbolistaNivel3 < futbolistasNivel3.length) {
+        mostrarFutbolistaNivel3();
+    } else {
+        mostrarPantallaFinalNivel3();
+    }
+}
+
+function mostrarPantallaFinalNivel3() {
+    ocultarMostrarPantallas("pantalla-juego-nivel-3", "pantalla-final-nivel-3");
+
+    document.getElementById("numCorrectasNivel3").innerText = intentosCorrectosNivel3;
+    document.getElementById("numIncorrectasNivel3").innerText = intentosIncorrectosNivel3;
+
+    const mensajeFinalNivel3 = document.createElement("p");
+
+    if (intentosCorrectosNivel3 >= 9) {
+        mensajeFinalNivel3.textContent += " ¡Eres un maestro en fútbol! ¡Felicidades!";
+    } else if (intentosCorrectosNivel3 >= 5) {
+        mensajeFinalNivel3.textContent += " ¡Excelente desempeño! Tu conocimiento de fútbol es notable.";
+    } else {
+        mensajeFinalNivel3.textContent += " ¡Sigue practicando! Siempre hay más por aprender.";
+    }
+
+    document.getElementById("pantalla-final-nivel-3").appendChild(mensajeFinalNivel3);
+}
+
+function volverAlInicioNivel3() {
+    indiceFutbolistaNivel3 = 0;
+    intentosCorrectosNivel3 = 0;
+    intentosIncorrectosNivel3 = 0;
+
+    ocultarMostrarPantallas("pantalla-final-nivel-3", "pantalla-inicial");
+}
+
+
 // Event Listeners
+
 document.getElementById("btnComenzarNivel1").addEventListener("click", comenzarJuegoNivel1);
 document.getElementById("btnComprobarNivel1").addEventListener("click", (event) => {
     const opElegida = event.target.dataset.opcion;
@@ -246,4 +361,9 @@ document.getElementById("btnComenzarNivel2").addEventListener("click", comenzarJ
 document.getElementById("btnComprobarNivel2").addEventListener("click", comprobarRespuestaNivel2);
 document.getElementById("btnVolverAlInicioNivel2").addEventListener("click", volverAlInicioNivel2);
 
+document.getElementById("btnIrAlNivel3").addEventListener("click", irAlSiguienteNivel3);
+
+document.getElementById("btnComenzarNivel3").addEventListener("click", comenzarJuegoNivel3);
+document.getElementById("btnComprobarNivel3").addEventListener("click", comprobarRespuestaNivel3);
+document.getElementById("btnVolverAlInicioNivel3").addEventListener("click", volverAlInicioNivel3);
 
